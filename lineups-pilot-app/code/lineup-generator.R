e40a71fe5916167decab97f0bea6c020c4bd0a58
Susan Vanderplas <srvanderplas@gmail.com>
2020-09-13 10:50:55 -0500

    add some comments

--- lineups-pilot-app/code/lineup-generator.R
+++ lineups-pilot-app/code/lineup-generator.R
@@ -13,24 +13,46 @@ library(digest)
 #   return(expData)
 # }
 
+
 expSim <- function(xMid, sigma, N = 20, xRange = c(0,20), yRange = c(1,100)){
-  
+
+  # This creates the line y = -x (scaled to fit the x and y ranges)
+  # |*            0
+  # |  *
+  # |    *
+  # |      *
+  # |        1
+  # |          2
+  # |0___________3
+  #
+  # where 1, 2, 3 represent different points used to determine the line curvature
+  #
   lineData   <- tibble(xLine = seq(xRange[1],xRange[2],0.1),
-                       yLine = ((yRange[1]-yRange[2])/(xRange[2]-xRange[1]))*(xLine-xRange[1])+yRange[2])
+                       yLine = (abs(diff(yRange))/abs(diff(xRange)))*(xLine-xRange[1])+yRange[2])
   pointsData <- tibble(xPoint = c(xRange[1], (xMid-0.1), (xMid+0.1), xRange[2]),
                        yPoint = c(yRange[1], lineData$yLine[lineData$xLine == xMid], lineData$yLine[lineData$xLine == xMid], yRange[2]))
-  
-  
+
+  # Connecting the 0 points in the illustration above with the 3rd point that
+  # determines curvature gives us a set of 3 points to use to fit an exponential
+  # line to the data.
+
+  # We fit a linear regression to the log-transformed data to get starting values
   lm.fit <- lm(log(yPoint) ~ xPoint, data = pointsData)
+
   alpha.0  <- exp(coef(lm.fit)[1]) %>% as.numeric()
   beta.0   <- coef(lm.fit)[2] %>% as.numeric()
-  theta.0 <- min(pointsData$yPoint) * 0.5  
+  theta.0 <- min(pointsData$yPoint) * 0.5  # Why 0.5?
+
+  # and then use NLS to fit a better line to the data
   start <- list(alpha = alpha.0, beta = beta.0, theta = theta.0)
-  nonlinear.fit   <- nls(yPoint ~ alpha * exp(beta * xPoint) + theta , data = pointsData, start = start)
+  nonlinear.fit   <- nls(yPoint ~ alpha * exp(beta * xPoint) + theta ,
+                         data = pointsData, start = start)
+
   alpha <- (coef(nonlinear.fit)[1] %>% as.numeric())/(exp((sigma^2)/2))
   beta <- coef(nonlinear.fit)[2] %>% as.numeric()
   theta <- coef(nonlinear.fit)[3] %>% as.numeric()
-  
+
+  # Then the coefficients are used to generate y values which are reasonable
   expData <- tibble(x = seq(xRange[1],xRange[2], length.out = N),
                     y = alpha*exp(beta*x + rnorm(N,0,sigma)) + theta)
   return(expData)
@@ -53,7 +75,7 @@ trtData <- expand_grid(target = seq(1,6,1),
            filter(curvature.target == curvature.null | (curvature.target != curvature.null & variability.target == variability.null), rorschach == 0) %>%
            expand_grid(set = seq(1,3,1)) %>%
            mutate(data_name = paste("set", set, "-", param_value, sep = ""))
-  
+
 panelData <- tibble("panel" = c("target", rep("null",19)),
                     ".n" = seq(0,19,1))
 
@@ -84,19 +106,19 @@ for(i in 1:nrow(trtData)){
   difficultyID <- trtData[i, "difficulty"] %>% as.numeric()
   pos          <- sample(1:20, 1)
 
-  lineupData <- lineup(true = simulatedData %>% 
+  lineupData <- lineup(true = simulatedData %>%
                                   filter(panel == "target", set == setID, param_value == paramID) %>%
-                                  select("x", "y"), 
-                       samples = simulatedData %>% 
+                                  select("x", "y"),
+                       samples = simulatedData %>%
                                  filter(panel == "null", set == setID, param_value == paramID) %>%
-                                 select("x", "y", ".n"), 
+                                 select("x", "y", ".n"),
                        pos = pos)
-  
+
   write.csv(lineupData, file = paste("csv_files/", dataID, ".csv", sep = ""), row.names = F)
 
-  linearPlot <- ggplot(lineupData, aes(x=x, y=y)) + 
+  linearPlot <- ggplot(lineupData, aes(x=x, y=y)) +
                   facet_wrap(~.sample, ncol=5) +
-                  geom_point() + 
+                  geom_point() +
                   theme(aspect.ratio = 1) +
                   theme_bw() +
                   theme(axis.title.y = element_blank(),
@@ -104,23 +126,23 @@ for(i in 1:nrow(trtData)){
                         axis.text.y  = element_blank(),
                         axis.text.x  = element_blank(),
                        )
-  
+
   save_lineup(linearPlot, file = linearID, path = "plots")
-  
-  logPlot <- ggplot(lineupData, aes(x=x, y=y)) + 
+
+  logPlot <- ggplot(lineupData, aes(x=x, y=y)) +
     facet_wrap(~.sample, ncol=5) +
-    geom_point() + 
+    geom_point() +
     theme(aspect.ratio = 1) +
     theme_bw() +
     theme(axis.title.y = element_blank(),
           axis.title.x = element_blank(),
           axis.text.y  = element_blank(),
           axis.text.x  = element_blank(),
-    ) + 
+    ) +
     scale_y_continuous(trans = "log10")
-  
+
   save_lineup(logPlot, file = logID, path = "plots")
-  
+
 picture_details[i, "sample_size"]       <- 20
 picture_details[i, "param_value"]       <- paramID
 picture_details[i, "p_value"]           <- 1
