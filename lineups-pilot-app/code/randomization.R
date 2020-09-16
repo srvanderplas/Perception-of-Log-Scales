e7895ed95ee8bf4a2da605637d18481321cfb01f
Emily Robinson <emily.robinson@huskers.unl.edu>
2020-09-15 23:02:38 -0500

    Set up sampling scheme in randomization file to randomly assign each participant 6 different param_values on both the log and linear scale.

--- lineups-pilot-app/code/randomization.R
+++ lineups-pilot-app/code/randomization.R
@@ -1,2 +1,11 @@
-pic_ids <- sample(1:108, 12)
+picture_details_randomization <- readr::read_csv(here("lineups-pilot-app", "plots", "picture-details.csv"))
+picture_details_randomization$set <- rep(c(1,1,2,2,3,3),18) 
+unique_param_values <- unique(picture_details_randomization$param_value)
+block_ids <- tibble(param_id = sample(1:18,6)) %>%
+             expand_grid(test_param = c("linear", "log")) %>%
+             mutate(param_value = unique_param_values[param_id],
+                    set  = sample(1:3,12, replace = T))
+
+joinCols = c("test_param", "param_value", "set")
+pic_ids <- sample(right_join(picture_details_randomization, block_ids, by = joinCols)$pic_id,12)
 trial_pic_ids <- sample(1:10, size = 2)
