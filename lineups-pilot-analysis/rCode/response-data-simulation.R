0529bfc95fc7efa5eadb059b98b1ba24269074e4
Emily Robinson <emily.robinson@huskers.unl.edu>
2020-10-28 13:26:54 -0500

    attempted simulating responses.

--- lineups-pilot-analysis/rCode/response-data-simulation.R
+++ lineups-pilot-analysis/rCode/response-data-simulation.R
@@ -1,8 +1,13 @@
 library(here)
+library(tidyverse)
+load("lineups-pilot-analysis/data/sim_response_data.rda")
 
-substrRight <- function(x, n){
-  substr(x, nchar(x)-n+1, nchar(x))
-}
+# --------------------------------------------------------------------------
+# Simulate Response Data----------------------------------------------------
+# --------------------------------------------------------------------------
+nparticipants = 50
+
+# funciton to simulate one individual's plots --------------------------------------------------
 
 samplingPicID <- function(participant, num_param_values = 9, param_values_per_participant = 6, num_sets = 2){
     
@@ -21,15 +26,126 @@ samplingPicID <- function(participant, num_param_values = 9, param_values_per_pa
     pic_ids <- sample(right_join(picture_details_randomization, param_block_ids, by = joinCols)$pic_id, 2*(param_values_per_participant))
 }
 
-sampleResponse <- function(participant){
-  sample(0:1,1)
+# set up participant's plot ids --------------------------------------------------
+
+picture_details <- readr::read_csv(here("lineups-pilot-app", "plots", "picture-details.csv"))
+set.seed(56156)
+sim_response_data <- tibble(nick_name = 1:nparticipants) %>%
+                     mutate(pic_id = map(nick_name, samplingPicID)) %>%
+                     unnest(pic_id) %>%
+                     full_join(picture_details, by = c("pic_id")) %>%
+                     mutate(pic_id             = factor(pic_id),
+                            test_param         = factor(test_param),
+                            data_name          = factor(data_name),
+                            pic_name           = factor(pic_name),
+                            target_curvature   = factor(substr(param_value,8,8), levels = c("E", "M", "H")),
+                            target_variability = factor(substr(param_value,10,11), levels = c("Lv")),
+                            null_curvature     = factor(substr(param_value,18,18), levels = c("E", "M", "H")),
+                            null_variability   = factor(substr(param_value,20,21), levels = c("Lv")),
+                            rorschach          = factor(substr(param_value,24,24), levels = c("0", "1"))
+                            ) %>%
+                     filter(rorschach == 0)
+
+# Set-up eta effects ------------------------------------------
+
+set.seed(56156)
+unique_phat_ijk <- sim_response_data %>%
+  select(target_curvature, null_curvature, test_param) %>%
+  unique() %>%
+  mutate(phat_ijk = map(target_curvature, function(target_curvature){phat_ijk = runif(1, min = 0.1, max = 0.9)})) %>%
+  unnest(phat_ijk)
+
+unique_nickname_l <- sim_response_data %>%
+  select(nick_name) %>%
+  unique() %>%
+  mutate(nick_name_l = map(nick_name, function(nick_name){nick_name_l = rnorm(1, 0, 0.23)})) %>%
+  unnest(nick_name_l)
+
+unique_dataname_m <- sim_response_data %>%
+  select(data_name) %>%
+  unique() %>%
+  mutate(data_name_m = map(data_name, function(data_name){data_name_m = rnorm(1, 0, 0.12)})) %>%
+  unnest(data_name_m)
+
+unique_wp_ijlm <- sim_response_data %>%
+  select(target_curvature, null_curvature, nick_name, data_name) %>%
+  unique() %>%
+  mutate(wp_ijlm = map(data_name, function(data_name){wp_ijlm = rnorm(1, 0, 0.018)})) %>%
+  unnest(wp_ijlm)
+
+# funciton to simulate response ---------------------------------------------
+
+# i = target curvature
+# j = null curvature
+# k = scale
+# l = nick name / participant
+# m = dataset
+
+sampleResponse <- function(phat_ijk, nick_name_l, data_name_m, wp_ijlm){
+  eta = log(phat_ijk/(1-phat_ijk)) + nick_name_l + data_name_m + wp_ijlm
+  p = 1/(1+exp(-eta))
+  rbinom(1, 1, p)
 }
 
-sim_response_data <- tibble(participant = 1:5) %>%
-                     mutate(pic_id = map(participant, samplingPicID)) %>%
-                     unnest(pic_id) %>%
-                     mutate(correct = map(participant, sampleResponse)) %>%
-                     unnest(correct) %>%
-                     full_join(picture_details_randomization, by = c(pic_id))
-sim_response_data
+# combine eta effects into the simulated dataset for response simulation ----------------------------
 
+sim_response_data2 <- sim_response_data %>%
+                      full_join(unique_phat_ijk, by = c("target_curvature", "null_curvature", "test_param")) %>%
+                      full_join(unique_nickname_l, by = c("nick_name")) %>%
+                      full_join(unique_dataname_m, by = c("data_name")) %>%
+                      full_join(unique_wp_ijlm, by = c("target_curvature", "null_curvature", "nick_name", "data_name")) %>%
+                      mutate(correct = pmap(list(phat_ijk, nick_name_l, data_name_m, wp_ijlm), sampleResponse)) %>%
+                      unnest(correct)
+
+# final simualted data ------------------------------------------------------------------------------
+
+sim_response_data <- sim_response_data2
+save(sim_response_data, file = "lineups-pilot-analysis/data/sim_response_data.rda")
+write.csv(sim_response_data, file = "lineups-pilot-analysis/data/sim_response_data.csv", row.names = F)
+
+# --------------------------------------------------------------------------
+# Create Labeler -----------------------------------------------------------
+# --------------------------------------------------------------------------
+
+target_curvature.labs <- c("Target: Lots of Curvature", "Target: Medium Curvature", "Target: Little Curvature")
+names(target_curvature.labs) <- c("E", "M", "H")
+
+null_curvature.labs <- c("Null: Lots of Curvature", "Null: Medium Curvature", "Null: Little Curvature")
+names(null_curvature.labs) <- c("E", "M", "H")
+
+global_labeller <- labeller(
+  target_curvature = target_curvature.labs,
+  null_curvature = null_curvature.labs)
+
+# --------------------------------------------------------------------------
+# Plot Simulated Data ------------------------------------------------------
+# --------------------------------------------------------------------------
+
+p_curvature <- sim_response_data %>%
+  ggplot(aes(x = test_param, y = correct, group = test_param, color = test_param)) +
+  # geom_jitter(width = 0.15, height = 0.15, alpha = 0.9) +
+  geom_point(position = position_jitterdodge(jitter.width = 0.3, jitter.height = 0.1, dodge.width = 1), alpha = 0.5) +
+  theme_bw() +
+  facet_grid(
+    null_curvature~target_curvature,
+    labeller = global_labeller
+  ) +
+  theme(aspect.ratio = 1) +
+  scale_y_continuous("Target panel detected", breaks = c(0,1)) +
+  scale_x_discrete("Target panel curvature embeded in null panel curvature") +
+  scale_color_brewer(name = "Scale", labels = c("Linear", "Log"), palette = "Paired")
+p_curvature
+
+# --------------------------------------------------------------------------
+# GLM model ----------------------------------------------------------------
+# --------------------------------------------------------------------------
+library(lme4)
+names(sim_response_data)
+
+glm.mod <- glmer(correct ~ target_curvature*null_curvature*test_param +
+                           (1 | nick_name) +
+                           (1 | data_name) +
+                           (target_curvature:null_curvature | nick_name:data_name),
+                 family = binomial(link = "logit"),
+                 data = sim_response_data
+                )
---
+++ lineups-pilot-analysis/sasCode/sim-response-glmm-analysis.sas
@@ -0,0 +1,24 @@
+*CLEARS SAS LOG AND RESULTS FOR CLEANER WORKING ENVIRONMENT;
+dm "log; clear; odsresults; clear;";
+
+PROC IMPORT
+	DATAFILE = 'C:\Users\ERobi\Documents\GitHub\Perception-of-Log-Scales\lineups-pilot-analysis\data\sim_response_data.csv'
+	OUT = sim_lineup_data
+	REPLACE;
+	GUESSINGROWS = 50;
+RUN;
+
+TITLE "Simulated Lineup Results Data";
+PROC PRINT DATA = sim_lineup_data (OBS = 10) NOOBS;
+RUN;
+
+TITLE "Simulated Lineup Model";
+PROC GLIMMIX DATA = sim_lineup_data;
+	CLASS 	nick_name data_name pic_id test_param param_value target_curvature null_curvature;
+	MODEL 	correct = test_param|target_curvature|null_curvature / D = Binomial LINK = logit;
+	RANDOM	intercept / SUBJECT = nick_name;
+	RANDOM	intercept / SUBJECT = data_name;
+	RANDOM	target_curvature*null_curvature / SUBJECT = nick_name*data_name;
+	LSMEANS	test_param*target_curvature*null_curvature / PLOT = MEANPLOT(SLICEBY = test_param CL ILINK JOIN) ILINK CL LINES;
+	NLOPTIONS MAXITER = 100;
+RUN;
