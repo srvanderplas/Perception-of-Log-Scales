40a0e66e339ea53974a76826e761057ade31c7b4
Susan Vanderplas <srvanderplas@gmail.com>
2020-11-23 13:01:46 -0600

    fix svg typo.

--- lineups-pilot-app/code/save_lineups.R
+++ lineups-pilot-app/code/save_lineups.R
@@ -37,21 +37,21 @@ save_lineup <- function(plot, filename, path = "plots", script = "www/js/action.
   ggsave(filename = filepaths[2], plot = plot, device = ext[2], ...)
 
   print(plot)
-  
+
   # This only works for 5 cols 4 rows
   name_corresp <- tribble(
     ~panelname, ~replacement,
-    "panel-1-1.8-5-8-5", "1", "panel-5-1.8-9-8-9", "2", "panel-4-2.8-13-8-13", "3", "panel-3-3.8-17-8-17", "4", "panel-2-4.8-21-8-21", "5", 
+    "panel-1-1.8-5-8-5", "1", "panel-5-1.8-9-8-9", "2", "panel-4-2.8-13-8-13", "3", "panel-3-3.8-17-8-17", "4", "panel-2-4.8-21-8-21", "5",
     "panel-2-1.13-5-13-5", "6", "panel-1-2.13-9-13-9", "7", "panel-5-2.13-13-13-13", "8", "panel-4-3.13-17-13-17", "9", "panel-3-4.13-21-13-21", "10",
-    "panel-3-1.18-5-18-5", "11", "panel-2-2.18-9-18-9", "12", "panel-1-3.18-13-18-13", "13", "panel-5-3.18-17-18-17", "14", "panel-4-4.18-21-18-21", "15", 
-    "panel-4-1.23-5-23-5", "16", "panel-3-2.23-9-23-9", "17", "panel-2-3.23-13-23-13", "23", "panel-1-4.23-17-23-17", "19", "panel-5-4.23-21-23-21", "20")
+    "panel-3-1.18-5-18-5", "11", "panel-2-2.18-9-18-9", "12", "panel-1-3.18-13-18-13", "13", "panel-5-3.18-17-18-17", "14", "panel-4-4.18-21-18-21", "15",
+    "panel-4-1.23-5-23-5", "16", "panel-3-2.23-9-23-9", "17", "panel-2-3.23-13-23-13", "18", "panel-1-4.23-17-23-17", "19", "panel-5-4.23-21-23-21", "20")
   namerepl <- c(name_corresp$replacement) %>% magrittr::set_names(name_corresp$panelname)
   grobs <- grid::grid.ls(grid::grid.force())
-  
-  
-  
+
+
+
   toggle <- "toggle"
-  
+
   idx <- grep("panel-", grobs$name)
   for (i in idx) {
     grid.garnish(grobs$name[i],
@@ -60,7 +60,7 @@ save_lineup <- function(plot, filename, path = "plots", script = "www/js/action.
                  onmouseout = paste("deframe('",grobs$name[i + 2], ".1')", sep = ""),
                  onmousedown = paste(sprintf("%shigh(evt, '", toggle),grobs$name[i + 2], ".1')", sep = ""))
   }
-  
+
   # Include script with each SVG
   # This may cause problems with shiny (not sure)
   # jsfile <- paste(readLines(script), collapse = "\n")
