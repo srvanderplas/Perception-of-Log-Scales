91076f93e0e650e333a38035b11bbc731df4a5e4
Emily Robinson <emily.robinson@huskers.unl.edu>
2021-04-07 15:03:50 -0500

    burning things to the ground...

--- you-draw-it-development/you-draw-it-pilot-app/code/randomization.R
+++
@@ -1,4 +0,0 @@
-randomization_dataset <- tibble(parm_id = c(seq(1,4,1),seq(1,4,1), c("S","F","V","N")),
-                                linear  = c(rep("true",4), rep("false",4), rep("true",4))
-                                )
-parm_ids <- sample(seq(1,12), 12, replace = F)
