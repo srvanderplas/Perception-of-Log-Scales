93580b4b578ef862391d3161d3656fc07184f3a6
Emily Robinson <emily.robinson@huskers.unl.edu>
2022-01-23 14:27:50 -0600

    added two population questions

--- estimation-development/estimation-app-dev/server.R
+++ estimation-development/estimation-app-dev/server.R
@@ -9,24 +9,36 @@ library(nlme)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    # Single Population
-    
+    # --------------------------------------------------------------------------
+    # One Population -----------------------------------------------------------
+    # --------------------------------------------------------------------------
+  
+    # Output simulated data set
     onePop_data <- reactive({
       
       set.seed(56156)
+      
+      # simulate data with multiplicative errors
       if (input$multiplicativeErrors){
+        
         data <- tibble(x = seq(input$yearRange[1], input$yearRange[2], 1),
                        y = input$alpha0*exp(input$beta0*(x-input$yearRange[1]) + rnorm(length(x), mean = 0, sd = input$sigma0)) + input$theta0,
                        y0 = input$alpha0*exp(input$beta0*(x-input$yearRange[1])) + input$theta0) %>%
                 mutate(x0 = x - input$yearRange[1])
+        
+      # simulate data with additive errrors
       } else if (!input$multiplicativeErrors){
+        
         data <- tibble(x = seq(input$yearRange[1], input$yearRange[2], 1),
                        y = input$alpha0*exp(input$beta0*(x-input$yearRange[1])) + input$theta0 + rnorm(length(x), mean = 0, sd = input$sigma0),
                        y0 = input$alpha0*exp(input$beta0*(x-input$yearRange[1])) + input$theta0) %>%
           mutate(x0 = x - input$yearRange[1])
+        
       }
       
+      # fit a trendline to the data using nls
       if (input$trendline) {
+        
         theta0 <- min(data$y0)
         lm0 <- lm(log(y - theta0) ~ x0, data = data)
         alpha0 <- exp(coef(lm0)[1])
@@ -36,11 +48,14 @@ shinyServer(function(input, output) {
                        data = data,
                        start = start0)
         data$yhat = fitted(nls.mod)
+        
       }
       
       data
+      
     })
     
+    # output nls coefficients
     output$nlsCoef_table <- renderText({
       
       data <- onePop_data()
@@ -58,12 +73,7 @@ shinyServer(function(input, output) {
       }
     })
     
-    # SET UP Y AXIS TICK MARKS
-    yMax <- reactive({
-      data <- onePop_data()
-      round_any(max(data$y), 1000, f = ceiling)
-    })
-    
+    # Set up y-axis range and tickmarks
     yMin <- reactive({
       data <- onePop_data()
       
@@ -73,6 +83,11 @@ shinyServer(function(input, output) {
         1
       }
       
+    })
+    
+    yMax <- reactive({
+      data <- onePop_data()
+      round_any(max(data$y), 1000, f = ceiling)
     })
     
     yTicks <- reactive({
@@ -103,10 +118,12 @@ shinyServer(function(input, output) {
       )
     })
     
+    # Output scatterplot of data
     output$onePop_plot <- renderPlot({
       
       data <- onePop_data()
       
+      # create base scatterplot
       basePlot <- data %>%
         ggplot(aes(x = x, y = y)) +
         geom_point() +
@@ -117,37 +134,123 @@ shinyServer(function(input, output) {
               )+
         scale_x_continuous("Year", expand = c(0.01,0.01))
       
+      # add the trendline if specified
       if(input$trendline){
+        
         basePlot <- basePlot +
           geom_line(aes(y = yhat), color = "blue", size = 1) +
           geom_line(aes(y = y0), color = "orange2", size = 1)
+        
       }
       
+      # adjust y scale based on specifications (linear/log, range, tick marks)
       if (input$scale == "Linear"){
+        
         basePlot +
           scale_y_continuous("Population", 
                              limits = c(input$popMin, input$popMax),
                              breaks = seq(0,input$popMax,input$tickmarks),
                              labels = comma)
-          # scale_y_continuous("Population", labels = comma)
+        
       } else if (input$scale == "Log10"){
+        
         basePlot +
           scale_y_continuous("Population", 
                              limits = c(input$popMin, input$popMax), 
                              trans = "log10",
                              breaks = 10^seq(0,10000,input$tickmarks),
-                             labels = comma
-                             )
+                             labels = comma)
+        
       } else if (input$scale == "Log2"){
+        
         basePlot +
           scale_y_continuous("Population", 
                              limits = c(input$popMin, input$popMax), 
                              trans = "log2",
                              breaks = 2^seq(0,10000,input$tickmarks),
-                             labels = comma
-                             )
+                             labels = comma)
       }
-
+    })
+    
+    # --------------------------------------------------------------------------
+    # Two Populations ----------------------------------------------------------
+    # --------------------------------------------------------------------------
+    
+    twoPop_data <- reactive({
+      
+      set.seed(56156)
+      datapoints <- tibble(x = seq(3000, 3050, 1),
+                           pop1 = input$alpha1*exp(input$beta1*(x-3000) + rnorm(length(x), 0, input$sigma1)) + input$theta1,
+                           pop2 = input$alpha2*exp(input$beta2*(x-3000) + rnorm(length(x), 0, input$sigma2)) + input$theta2,
+                     ) %>%
+        pivot_longer(cols = c("pop1", "pop2"),
+                     names_to = "group",
+                     values_to = "population")
+      
+      datalines <- tibble(x = seq(3000, 3050, 1),
+                          pop1 = input$alpha1*exp(input$beta1*(x-3000)) + input$theta1,
+                          pop2 = input$alpha2*exp(input$beta2*(x-3000)) + input$theta2
+      ) %>%
+        pivot_longer(cols = c("pop1", "pop2"),
+                     names_to = "group",
+                     values_to = "trendline")
+      
+      data <- full_join(datapoints, datalines, by = c("x", "group"))
+      
+      data
+      
+    })
+    
+    output$twoPop_plot <- renderPlot({
+      
+      data <- twoPop_data()
+      
+      baseplot <- data %>%
+        ggplot(aes(x = x, y = population, color = group, group = group)) +
+        geom_point() +
+        theme_bw() +
+        theme(aspect.ratio = 1,
+              axis.title = element_text(size = 14),
+              axis.text = element_text(size = 12),
+              legend.text = element_text(size = 12),
+              legend.title = element_text(size = 14),
+              legend.position = "bottom"
+        ) +
+        scale_x_continuous("Year") +
+        scale_color_manual(values = c("navy", "orange2"))
+      
+      if(input$trendline2){
+        
+        baseplot <- baseplot + 
+          geom_line(aes(y = trendline), size = 1)
+        
+      }
+      
+      if (input$scale2 == "Linear"){
+        
+        baseplot + 
+          scale_y_continuous("Population", labels = comma)
+        
+      } else if (input$scale2 == "Log10"){
+        
+        baseplot + 
+          scale_y_continuous("Population", 
+                             labels = comma,
+                             trans = "log10",
+                             breaks = 10^seq(0,10000,1)
+                             )
+        
+      } else if (input$scale2 == "Log2"){
+        
+        baseplot + 
+          scale_y_continuous("Population", 
+                             labels = comma,
+                             trans = "log2",
+                             breaks = 2^seq(0,10000,1)
+                             )
+        
+      }
+      
     })
 
 })
--- estimation-development/estimation-app-dev/ui.R
+++ estimation-development/estimation-app-dev/ui.R
@@ -12,12 +12,19 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("Estimation Task Development"),
 
-    # Sidebar with a slider input for number of bins
+    # --------------------------------------------------------------------------
+    # Sidebar ------------------------------------------------------------------
+    # --------------------------------------------------------------------------
+    
     sidebarLayout(
         sidebarPanel(
             selectInput("numPops",
                         "Number of Populations",
                         choices = c("One", "Two")),
+            
+            # ------------------------------------------------------------------
+            # ONE POPULATION ---------------------------------------------------
+            # ------------------------------------------------------------------
             
             conditionalPanel("input.numPops == 'One'",
                              
@@ -51,75 +58,237 @@ shinyUI(fluidPage(
                            max = 100),
               textInput("qI4",
                         "Between 3030 and 3040, how does the population of creatures change? (open-ended; look for wording)")
+            ),
+            
+            # ------------------------------------------------------------------
+            # TWO POPULATIONS --------------------------------------------------
+            # ------------------------------------------------------------------
+            
+            conditionalPanel("input.numPops == 'Two'",
+                             
+              helpText(h5("Elementary: literal reading of the data")),
+
+              numericInput("q2E3",
+                           "What is the population when both groups are equal?",
+                           value = NA,
+                           min = 0,
+                           max = 10000000),
+              
+              numericInput("q2E3",
+                           "How many years does it take for the population of group 1 to equal the population of group 2?",
+                           value = NA,
+                           min = 0,
+                           max = 100),
+              
+              helpText(h5("Intermediate: reading between the data")),
+              
+              numericInput("q2I1",
+                           "How many more creatures did group 2 have than group 1 in 3020? (additive)",
+                           value = NA,
+                           min = 0,
+                           max = 10000000),
+              
+              numericInput("q2I2",
+                           "How many times larger was the starting population of group 2 than group 1? (multiplicative)",
+                           value = NA,
+                           min = 0,
+                           max = 10000000),
+              
+              textInput("q2I3",
+                        "Considering the two curves of the graph only as marks on a piece of paper, how do the changes in these two curves compare? (open-ended)"
+                        ),
+              
+              helpText(h5("Advanced: reading beyond the data")),
+              
+              textInput("q2A1",
+                        "From 3000 to 3020, what which population had a higher reproductive rate?"
+                        ),
+              
+              textInput("q2A1",
+                        "Which population do you expect to be larger in 3100?"
+              ),
             )
         ),
+        
+    # --------------------------------------------------------------------------
+    # MAIN ---------------------------------------------------------------------
+    # --------------------------------------------------------------------------
+    
+    mainPanel(
+      
+      # ------------------------------------------------------------------------
+      # ONE POPULATION ---------------------------------------------------------
+      # ------------------------------------------------------------------------
+      conditionalPanel("input.numPops == 'One'",
+        
+        # Output Plot ----------------------------------------------------------
+        
+        column(width = 7,
+          helpText(h4("Scenario: words go here.")),
+          plotOutput("onePop_plot", height = "500px"),
+          verbatimTextOutput("nlsCoef_table")
+        ),
+        
+        # Data & Plot Specifications -------------------------------------------
+        
+        column(width = 5,
+               helpText(h4("Data Simulation")),
+               withMathJax(helpText("$$\\text{population} = \\alpha\\cdot e^{\\beta\\cdot (\\text{year} - \\text{min year})} + \\theta$$")),
+               checkboxInput("multiplicativeErrors",
+                             "Multiplicative Errors",
+                              value = T),
+                  
+             fluidRow(
+               column(width = 3,
+                  numericInput("alpha0", 
+                               "$$\\alpha$$",
+                               value = 5,
+                               min = 1, 
+                               max = 50)
+                ),
+                column(width = 3,
+                       numericInput("beta0", 
+                                    "$$\\beta$$",
+                                     value = 0.15,
+                                     min = 0, 
+                                     max = 5,
+                                     step = 0.05)
+                 ),
+                 column(width = 3,
+                        numericInput("theta0", 
+                                     "$$\\theta$$",
+                                     value = 30,
+                                     min = -50, 
+                                     max = 500)
+                 ),
+                 column(width = 3,
+                        numericInput("sigma0", 
+                                     "$$\\sigma$$",
+                                     value = 0.25,
+                                     min = 0, 
+                                     max = 3,
+                                     step = 0.05)
+                 )
+               ),
+              
+              sliderInput("yearRange", 
+                          "Year Range", 
+                           min = 3000,
+                           max = 3100,
+                           value = c(3000, 3050)),
+                 
+               helpText(h4("Graph Aesthetics")),
+               selectInput("scale",
+                           "Scale",
+                           choices = c("Linear", "Log10", "Log2"),
+                           selected = "Linear"),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-          conditionalPanel("input.numPops == 'One'",
-            column(width = 7,
-              helpText(h4("Scenario: words go here.")),
-              plotOutput("onePop_plot", height = "500px"),
-              verbatimTextOutput("nlsCoef_table")
-            ),
-            column(width = 5,
-                   helpText(h4("Data Simulation")),
-                   withMathJax(helpText("$$\\text{population} = \\alpha\\cdot e^{\\beta\\cdot (\\text{year} - \\text{min year})} + \\theta$$")),
-                   checkboxInput("multiplicativeErrors",
-                               "Multiplicative Errors",
-                               value = T),
-                   
-                   fluidRow(
+               uiOutput("popRange"),
+             
+               checkboxInput("trendline",
+                             "Trendline",
+                             value = F)
+        )
+      ),
+      
+      # ------------------------------------------------------------------------
+      # TWO POPULATIONS ---------------------------------------------------------
+      # ------------------------------------------------------------------------
+      
+      conditionalPanel("input.numPops == 'Two'",
+                       
+          column(width = 7,
+                 helpText(h4("Scenario: words go here.")),
+                 plotOutput("twoPop_plot", height = "500px")
+                 ),
+          
+          column(width = 5,
+                 
+                 helpText(h4("Data Simulation")),
+                 
+                 withMathJax(helpText("$$\\text{population 1} = \\alpha_1\\cdot e^{\\beta_1\\cdot (\\text{year} - \\text{min year}) + \\epsilon} + \\theta_1$$")),
+                 
+                 fluidRow(
                    column(width = 3,
-                          numericInput("alpha0", 
-                                       "$$\\alpha$$",
+                          numericInput("alpha1", 
+                                       "$$\\alpha_1$$",
                                        value = 5,
                                        min = 1, 
                                        max = 50)
                    ),
                    column(width = 3,
-                          numericInput("beta0", 
-                                       "$$\\beta$$",
+                          numericInput("beta1", 
+                                       "$$\\beta_1$$",
                                        value = 0.15,
                                        min = 0, 
                                        max = 5,
-                                       step = 0.05)
+                                       step = 0.01)
                    ),
                    column(width = 3,
-                          numericInput("theta0", 
-                                       "$$\\theta$$",
+                          numericInput("theta1", 
+                                       "$$\\theta_1$$",
                                        value = 30,
                                        min = -50, 
-                                       max = 500),
+                                       max = 500)
                    ),
                    column(width = 3,
-                          numericInput("sigma0", 
-                                       "$$\\sigma$$",
+                          numericInput("sigma1", 
+                                       "$$\\sigma_1$$",
                                        value = 0.25,
                                        min = 0, 
                                        max = 3,
-                                       step = 0.05),
+                                       step = 0.05)
                    )
+                 ),
+                 
+                 withMathJax(helpText("$$\\text{population 2} = \\alpha_2\\cdot e^{\\beta_2\\cdot (\\text{year} - \\text{min year}) + \\epsilon} + \\theta_2$$")),
+                 
+                 fluidRow(
+                   column(width = 3,
+                          numericInput("alpha2", 
+                                       "$$\\alpha_2$$",
+                                       value = 5,
+                                       min = 1, 
+                                       max = 50)
                    ),
-                   
-                   sliderInput("yearRange", 
-                               "Year Range", 
-                               min = 3000,
-                               max = 3100,
-                               value = c(3000, 3050)),
-                   
-                   helpText(h4("Graph Aesthetics")),
-                   selectInput("scale",
-                               "Scale",
-                               choices = c("Linear", "Log10", "Log2"),
-                               selected = "Linear"),
-
-                   uiOutput("popRange"),
-                   checkboxInput("trendline",
-                                 "Trendline",
-                                 value = F)
-            )
+                   column(width = 3,
+                          numericInput("beta2", 
+                                       "$$\\beta_2$$",
+                                       value = 0.12,
+                                       min = 0, 
+                                       max = 5,
+                                       step = 0.01)
+                   ),
+                   column(width = 3,
+                          numericInput("theta2", 
+                                       "$$\\theta_2$$",
+                                       value = 300,
+                                       min = -50, 
+                                       max = 500)
+                   ),
+                   column(width = 3,
+                          numericInput("sigma2", 
+                                       "$$\\sigma_2$$",
+                                       value = 0.25,
+                                       min = 0, 
+                                       max = 3,
+                                       step = 0.05)
+                   )
+                 ),
+                 
+                 helpText(h4("Graph Aesthetics")),
+                 
+                 selectInput("scale2",
+                             "Scale",
+                             choices = c("Linear", "Log10", "Log2"),
+                             selected = "Linear"),
+                 
+                 checkboxInput("trendline2",
+                               "Trendline",
+                               value = T)
+                 
           )
-        )
+      )
     )
+  )
 ))
