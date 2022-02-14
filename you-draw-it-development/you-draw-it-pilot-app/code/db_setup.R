346439bdb7c5a1288aa7da08a59e10c6ea72a19d
Emily Robinson <emily.robinson@huskers.unl.edu>
2022-02-14 11:35:47 -0600

    fixed weird db saved locations.

--- you-draw-it-development/you-draw-it-pilot-app/code/db_setup.R
+++ you-draw-it-development/you-draw-it-pilot-app/code/db_setup.R
@@ -1,12 +1,10 @@
 library(RSQLite)
 library(DBI)
 library(tidyverse)
-library(patchwork)
-library(here)
 
 # Connect to data base ---------------------------------------------
 
-filename <- "estimation-development/estimation-pilot-app/estimation_data.db"
+filename <- "you-draw-it-development/you-draw-it-pilot-app/you_draw_it_data.db"
 sqlite.driver <- dbDriver("SQLite")
 db_con <- dbConnect(sqlite.driver, dbname = filename)
 dbListTables(db_con)
@@ -15,153 +13,47 @@ dbListTables(db_con)
 
 experiment_details <- dbReadTable(db_con,"experiment_details")
 # experiment_details <- experiment_details[0,]
-# experiment_details <- data.frame(experiment = "emily-log-estimation-pilot-app",
-#                                  num_qs      = 14,
-#                                  trials_req = 0
-#                                  )
-# 
+# experiment_details <- data.frame(experiment = "emily-you-draw-it-pilot-app",
+# question   = "Use your mouse to fill in the trend in the yellow box region.",
+# ydi_pp     = 12,
+# trials_req = 0
+# )
 # dbRemoveTable(db_con, "experiment_details")
 # dbWriteTable(db_con, "experiment_details", experiment_details)
 # experiment_details <- dbReadTable(db_con,"experiment_details")
 experiment_details
 
-# true_parameters --------------------------------------------------------------
+# Exponential Scale Study Parameter Details ------------------------
 
-true_parameters <- dbReadTable(db_con,"true_parmameters")
-# true_parameters <- tibble(xmin  = 3000,
-#                           xmax  = 3050,
-#                           xby   = 1,
-#                           alpha = 130,
-#                           beta  = 0.12,
-#                           theta = 50,
-#                           sigma = 1.5,
+exp_parameter_details <- dbReadTable(db_con,"exp_parameter_details")
+# exp_parameter_details <- data.frame(beta = c(0.1, 0.23), 
+#                                     sd = c(0.09, 0.25)) %>%
+#                           expand_grid(N = 30,
+#                                       x_min = 0,
+#                                       x_max = 20,
+#                                       x_by = 0.25
 #                           )
-# 
-# dbRemoveTable(db_con, "true_parameters")
-# dbWriteTable(db_con,  "true_parameters", true_parameters)
-# true_parameters <- dbReadTable(db_con, "true_parameters")
-true_parameters
+# dbRemoveTable(db_con, "exp_parameter_details")
+# dbWriteTable(db_con,  "exp_parameter_details", exp_parameter_details)
+# exp_parameter_details <- dbReadTable(db_con,"exp_parameter_details")
+# exp_parameter_details
 
-# simulated_data -----------------------------------------------------
+# Eye Fitting Parameter Details -----------------------------------
 
-simulated_data  <- dbReadTable(db_con,"simulated_data")
-true_parameters <- dbReadTable(db_con, "true_parameters")
+eyefitting_parameter_details <- dbReadTable(db_con,"eyefitting_parameter_details")
+# eyefitting_parameter_details <- tibble(parm_id = c("S", "F", "V", "N"),
+#                                        y_xbar = c(3.88, 3.9, 3.89, 4.11),
+#                                        slope  = c(0.66, 0.66, 1.98, -0.70),
+#                                        sigma  = c(1.3, 2.8, 1.5, 2.5),
+#                                        x_min   = c(0, 0, 4, 0),
+#                                        x_max   = c(20, 20, 16, 20)) %>%
+#                                 expand_grid(x_by = 0.25)
+# dbRemoveTable(db_con, "eyefitting_parameter_details")
+# dbWriteTable(db_con,  "eyefitting_parameter_details", eyefitting_parameter_details)
+# eyefitting_parameter_details <- dbReadTable(db_con,"eyefitting_parameter_details")
+eyefitting_parameter_details
 
-# set.seed(56156)
-# simulated_data <- tibble(x         = seq(true_parameters$xmin, true_parameters$xmax, true_parameters$xby),
-#                          dataset1  = true_parameters$alpha*exp(true_parameters$beta*(x - true_parameters$xmin + rnorm(length(x), mean = 0, sd = true_parameters$sigma))) + true_parameters$theta,
-#                          dataset2  = true_parameters$alpha*exp(true_parameters$beta*(x - true_parameters$xmin + rnorm(length(x), mean = 0, sd = true_parameters$sigma))) + true_parameters$theta,
-#                          y0        = true_parameters$alpha*exp(true_parameters$beta*(x - true_parameters$xmin)) + true_parameters$theta) %>%
-#   pivot_longer(cols = c("dataset1", "dataset2"),
-#                names_to = "dataset",
-#                values_to = "y") %>%
-#   arrange(dataset, x)
-# 
-# dbRemoveTable(db_con, "simulated_data")
-# dbWriteTable(db_con,  "simulated_data", simulated_data)
-# simulated_data <- dbReadTable(db_con, "simulated_data")
-simulated_data
-
-# plot simulated data
-base_plot <- simulated_data %>% 
-  ggplot(aes(x = x, y = y)) +
-  geom_point() +
-  geom_line(aes(y = y0), color = "steelblue") +
-  facet_grid(~scenario) +
-  theme_bw() +
-  theme(aspect.ratio = 1)
-
-linear_plot <- base_plot +
-  scale_y_continuous("Population",
-                     labels = comma,
-                     limits = c(100, 55000),
-                     breaks = seq(0, 55000, 5000),
-                     minor_breaks = c())
-
-log_plot <- base_plot + 
-  scale_y_continuous("Population",
-                     trans = "log2",
-                     breaks = 2^seq(0,10000,1),
-                     labels = comma,
-                     minor_breaks = c(2^seq(0,10000,1) + (2^seq(0,10000,1)/2), 2^seq(0,10000,1) + 3*(2^seq(0,10000,1)/4))
-                     # minor_breaks = 2^seq(0,10000,1) + (2^seq(0,10000,1)/2)
-                     # minor_breaks = seq(0, 55000, 5000)
-                     # minor_breaks = c(156, 312)
-                     # minor_breaks = c()
-  )
-
-final_plot <- linear_plot / log_plot
-final_plot
-# ggsave(final_plot, filename = "estimation-development/estimation-pilot-app/simulated-data-plots.png", width = 12, height = 12)
-
-# scanario_text_data -----------------------------------------------------------
-
-# Startrek (Tribbles)
-# stardate = 4523.3 (universe year = Fri Jan 11 2267 21:58:18)
-# https://trekguide.com/Stardates.htm
-# 1 week span: Jan 11, 2267 (4516.69) to Jan 18, 2267 (4567.19) = 50.5 stardates
-# 4516.69 - 3000 (+ 1516.69)
-# Let's round to 4500 (+ 1500)
-
-# Star Wars (Ewoks)
-# Galactic Civil War 0 - 5 ABY....
-# https://starwars.fandom.com/wiki/Timeline_of_galactic_history
-# Start at 0 ABY (After the Battle of Yavin) (- 3000)
-
-scenario_text_data <- dbReadTable(db_con, "scenario_text_data")
-# scenario_text_data <- tibble(creature = c("tribble", "ewok"),
-#                              text = c("Hi, we're Tribbles! We were taken from our native planet, Iota Germinorum IV, and brought abroad Starfleet in stardate 4500. A Starfleet scientist, Edward Larkin, genetically engineered us to increase our reproductive rate in an attempt to solve a planetary food shortage. <br> <br> The Tribble population on Starfleet over the next 50 Stardates (equivalent to 1 week universe time) is illustrated in the graph. We need your help answering a few questions regarding the population of Tribbles.",
-# 
-#                                       "Hi, we're Ewoks! We are native to the forest moon of Endor. After the Galactic Civil War, some Ewoks traveled offworld to help Rebel veterens as 'therapy Ewoks' and began to repopulate. <br> <br> The Ewok population After the Battle of Yavin (ABY) is illustrated in the graph. We need your help answering a few questions regarding the population of Ewoks offworld."))
-# 
-# dbRemoveTable(db_con, "scenario_text_data")
-# dbWriteTable(db_con, "scenario_text_data", scenario_text_data)
-# scenario_text_data <- dbReadTable(db_con,"scenario_text_data")
-scenario_text_data
-
-# estimation_questions ---------------------------------------------------------
-
-# Startrek (Tribbles)
-# stardate = 4523.3 (universe year = Fri Jan 11 2267 21:58:18)
-# https://trekguide.com/Stardates.htm
-# 1 week span: Jan 11, 2267 (4516.69) to Jan 18, 2267 (4567.19) = 50.5 stardates
-# 4516.69 - 3000 (+ 1516.69)
-# Let's round to 4500 (+ 1500)
-
-# Star Wars (Ewoks)
-# Galactic Civil War 0 - 5 ABY....
-# https://starwars.fandom.com/wiki/Timeline_of_galactic_history
-# Start at 0 ABY (After the Battle of Yavin) (- 3000)
-
-estimation_questions <- dbReadTable(db_con, "estimation_questions")
-# estimation_questions <- tibble(q_id = rep(c("scenario", "Q0", "QE1", "QE2", "QI1", "QI2", "QI3"),2),
-#                                creature = c(rep("tribble", 7), rep("ewok", 7)),
-# 
-#                                          # tribble scenario
-#                                qtext = c("tribble",
-#                                          "Between stardates 4530 and 4540, how does the population of Tribbles change?",
-#                                          "What is the population of Tribbles in stardate 4510?",
-#                                          "At what stardate does the population of Tribbles reach 4,000?",
-#                                          "From 4520 to 4540, the population increases by ____ Tribbles.",
-#                                          "How many times more Tribbles are there in 4540 than in 4520?",
-#                                          "How long does it take for the population of Tribbles in stardate 4510 to double?",
-# 
-#                                          # ewok scenario
-#                                          "ewok",
-#                                          "Between 30 and 40 ABY, how does the population of Ewoks change?",
-#                                          "What is the population of Ewoks in 10 ABY?",
-#                                          "In what ABY does the population of Ewoks reach 4,000?",
-#                                          "From 20 ABY to 40 ABY, the population increases by ____ Ewoks.",
-#                                          "How many times more Ewoks are there in 40 ABY than in 20 ABY?",
-#                                          "How long does it take for the population of Ewoks in 10 ABY to double?"
-#                                         )
-#                                )
-# dbRemoveTable(db_con, "estimation_questions")
-# dbWriteTable(db_con, "estimation_questions", estimation_questions)
-# estimation_questions <- dbReadTable(db_con,"estimation_questions")
-estimation_questions
-
-# Users Data -------------------------------------------------------------------
+# Users Data ---------------------------------------------------
 
 users <- dbReadTable(db_con,"users")
 # users <- tibble(nick_name       = "test",
@@ -178,27 +70,50 @@ users <- dbReadTable(db_con,"users")
 # users <- dbReadTable(db_con,"users")
 users
 
+
 # Feedback Drawn Data ---------------------------------------------------
 
-feedback <- dbReadTable(db_con, "feedback")
-# feedback <- tibble(ip_address = "test",
+feedback <- dbReadTable(db_con,"feedback")
+unique(feedback$parm_id)
+# feedback <- tibble(parm_id    = "test",
+#                    x          = NA,
+#                    y          = NA,
+#                    ydrawn     = NA,
+#                    linear     = NA,
+#                    ip_address = "test",
 #                    nick_name  = "test",
 #                    study_starttime = NA,
 #                    start_time = NA,
-#                    end_time   = NA,
-#                    order      = NA,
-#                    q_id       = NA,
-#                    creature   = "test",
-#                    dataset    = NA,
-#                    scale      = NA,
-#                    response   = "test"
+#                    end_time   = NA
 #                    )
 # feedback <- feedback[0,]
 # dbRemoveTable(db_con, "feedback")
 # dbWriteTable(db_con, "feedback", feedback)
 # feedback <- dbReadTable(db_con, "feedback")
 feedback
+feedback %>%
+  dplyr::filter(study_starttime == max(users$study_starttime)) %>%
+  select(parm_id, linear) %>%
+  unique()
 
-# Disconnect to data base ---------------------------------------------
+# Simulated Data ---------------------------------------------------
+simulated_data <- dbReadTable(db_con,"simulated_data")
+# simulated_data <- tibble(parm_id    = "test",
+#                          dataset    = NA,
+#                          x          = NA,
+#                          y          = NA,
+#                          ip_address = "test",
+#                          nick_name  = "test",
+#                          study_starttime = NA
+#                          )
+# simulated_data <- simulated_data[0,]
+# dbRemoveTable(db_con, "simulated_data")
+# dbWriteTable(db_con, "simulated_data", simulated_data)
+# simulated_data <- dbReadTable(db_con, "simulated_data")
+simulated_data
+simulated_data %>%
+  dplyr::filter(study_starttime == max(users$study_starttime)) %>%
+  select(parm_id) %>%
+  unique()
 
 dbDisconnect(db_con)
